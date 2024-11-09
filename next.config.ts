48d2262007f54b4b761a46991b7f9dbf2c501e5b
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-11-09 18:46:41 -0300

    De primera V 1.1.7

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
+import { env } from 'process';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['deprimera.vercel.app', env.SUPABASE_BUCKET_URL.replace('storage/v1/object/public/product-images/', '')],
+  },
 };
 
 export default nextConfig;
