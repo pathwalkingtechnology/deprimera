5bc5cec1720beb4fb88298cf40794458a39844a3
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-11-11 18:17:06 -0300

    De primera V 1.3.5

--- pages/admin/productos/[id].tsx
+++ pages/admin/productos/[id].tsx
@@ -124,7 +124,14 @@ const EditProduct = () => {
             type="file"
             id="imagen"
             accept="image/jpeg, image/png"
-            onChange={(e) => setImagen(e.target.files?.[0])}
+            onChange={(e) => {
+  const file = e.target.files?.[0];
+  if (file) {
+    setImagen(file);
+  } else {
+    setImagen(null);
+  }
+}}
             className="input-file"
           />
         </div>
